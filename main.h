c13976154902a5ab702af6e9f7d5bf215b49ee72
Prima2020 <ajibaretosin@gmail.com>
2023-10-19 01:19:58 -0700

    task 2

--- main.h
+++ main.h
@@ -9,5 +9,9 @@ int print_string(va_list val);
 int _strlen(char *str);
 int _strlenc(const char *str);
 int print_37(void);
+int print_unsigned_int(va_list args);
+int print_dec(va_list args);
+int print_bin(va_list val);
+int print_int(va_list args);
 
 #endif /* MAIN_H */
